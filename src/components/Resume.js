525f68a19c881cfb0c98f67f2fcb9103fb691765
Juan Camilo Restrepo <juan.restrepo.belalcazar@gmail.com>
2025-03-02 18:00:48 -0800

    Updated Resume

---
+++ src/components/Resume.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <section id="resume" className="resume">
+      <h2>Resume</h2>
+      <a
+        href="/assets/Jake_s_Resume_Data_Engineering.pdf"
+        download
+        className="download-btn"
+      >
+        Download Resume
+      </a>
+    </section>
+  );
+};
+
+export default Resume;
